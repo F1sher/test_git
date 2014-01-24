75b1c9c6aef8e7d5f1656c91a33a3331fc7325cd
F1sher <dasalam@gmail.com>
2014-01-24 21:59:21 +0400

    123 folder

---
+++ 123/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void function_in_file1();
+void function_in_file2();
+
+int main(int argc, char **argv)
+{
+	function_in_file1();
+	function_in_file2();
+	
+	printf("Ver 1.2\n");
+	
+	return 0;
+}
+
